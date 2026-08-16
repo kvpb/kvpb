041d5d3a75b5c7adfb26ecb0623ce5480985f80e
Karl <local-part@domain>
2026-08-16 20:30:40 +0200

    (kvpb/test/) test_helper.rb updated accordingly.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
-ENV["RAILS_ENV"] ||= "test"
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its
+#	documentation for any purpose and without fee is hereby granted, provided that
+#	the above copyright notice appear in all copies and that both that copyright
+#	notice and this permission notice appear in supporting documentation, and that
+#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+#	pertaining to distribution of the software without specific, written prior
+#	permission. Karl Vincent Pierre Bertin makes no representations about the
+#	suitability of this software for any purpose.  It is provided "as is" without
+#	express or implied warranty.
+
+ENV[ "RAILS_ENV" ] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require_relative "test_helpers/session_test_helper"
@@ -6,7 +18,7 @@ require_relative "test_helpers/session_test_helper"
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    parallelize( workers: :number_of_processors )
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
@@ -14,3 +26,10 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+#	test_helper.rb
+#	kvpb.fr
+#
+#	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+#	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+#	local-part@domain
