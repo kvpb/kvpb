e0ed557b79e8bde5eec0e815b38648c9c518ac6e
Karl <local-part@domain>
2026-08-16 20:30:11 +0200

    (kvpb/db/migrate/) 20260815224652_create_users.rb updated.

--- db/migrate/20260815224652_create_users.rb
+++ db/migrate/20260815224652_create_users.rb
@@ -1,4 +1,16 @@
-class CreateUsers < ActiveRecord::Migration[8.1]
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its
+#	documentation for any purpose and without fee is hereby granted, provided that
+#	the above copyright notice appear in all copies and that both that copyright
+#	notice and this permission notice appear in supporting documentation, and that
+#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+#	pertaining to distribution of the software without specific, written prior
+#	permission. Karl Vincent Pierre Bertin makes no representations about the
+#	suitability of this software for any purpose.  It is provided "as is" without
+#	express or implied warranty.
+
+class CreateUsers < ActiveRecord::Migration[ 8.1 ]
   def change
     create_table :users do |t|
       t.string :username, null: false
@@ -12,3 +24,10 @@ class CreateUsers < ActiveRecord::Migration[8.1]
     add_index :users, :email_address, unique: true
   end
 end
+
+#	20260815224652_create_users.rb
+#	kvpb.fr
+#
+#	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+#	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+#	local-part@domain
