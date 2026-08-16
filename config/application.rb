71213fc9ddc245823797bacdaf3e392731a8f0aa
Karl <local-part@domain>
2026-08-16 20:30:21 +0200

    (kvpb/config/) application.rb updated.

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,22 @@
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its
+#	documentation for any purpose and without fee is hereby granted, provided that
+#	the above copyright notice appear in all copies and that both that copyright
+#	notice and this permission notice appear in supporting documentation, and that
+#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+#	pertaining to distribution of the software without specific, written prior
+#	permission. Karl Vincent Pierre Bertin makes no representations about the
+#	suitability of this software for any purpose.  It is provided "as is" without
+#	express or implied warranty.
+
 require_relative "boot"
 
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require( *Rails.groups )
 
 module Kvpb
   class Application < Rails::Application
@@ -14,7 +26,7 @@ module Kvpb
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib( ignore: %w[assets tasks] )
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -25,3 +37,10 @@ module Kvpb
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+#	application.rb
+#	kvpb.fr
+#
+#	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+#	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+#	local-part@domain
