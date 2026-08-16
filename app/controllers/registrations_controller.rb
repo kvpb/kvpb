dc21698317c8d1f6e80a455c4b3a49ba88455396
Karl <local-part@domain>
2026-08-16 22:14:45 +0200

    (kvpb/app/controllers/) registrations_controller.rb updated: moved the copyright notice out of the header and into a second footer.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,3 @@
-#	Copyright 2026 by Karl Vincent Pierre Bertin
-#
-#	Permission to use, copy, modify, and distribute this software and its
-#	documentation for any purpose and without fee is hereby granted, provided that
-#	the above copyright notice appear in all copies and that both that copyright
-#	notice and this permission notice appear in supporting documentation, and that
-#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
-#	pertaining to distribution of the software without specific, written prior
-#	permission. Karl Vincent Pierre Bertin makes no representations about the
-#	suitability of this software for any purpose.  It is provided "as is" without
-#	express or implied warranty.
-
 class RegistrationsController < ApplicationController
   rate_limit to: 5, within: 1.hour, only: :create, with: -> { redirect_to new_registration_path, alert: "Try again later." }
 
@@ -49,3 +37,7 @@ end
 #	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
 #	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
 #	local-part@domain
+#
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its documentation for any purpose and without fee is hereby granted, provided that the above copyright notice appear in all copies and that both that copyright notice and this permission notice appear in supporting documentation, and that the name of Karl Vincent Pierre Bertin not be used in advertising or publicity pertaining to distribution of the software without specific, written prior permission. Karl Vincent Pierre Bertin makes no representations about the suitability of this software for any purpose. It is provided "as is" without express or implied warranty.
