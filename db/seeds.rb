51b25876f0ad0e2902df21e75d262479599514bf
Karl <local-part@domain>
2026-08-16 22:15:50 +0200

    (kvpb/db/) seeds.rb updated: moved the copyright notice out of the header and into a second footer.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-#	Copyright 2026 by Karl Vincent Pierre Bertin
-#
-#	Permission to use, copy, modify, and distribute this software and its
-#	documentation for any purpose and without fee is hereby granted, provided that
-#	the above copyright notice appear in all copies and that both that copyright
-#	notice and this permission notice appear in supporting documentation, and that
-#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
-#	pertaining to distribution of the software without specific, written prior
-#	permission. Karl Vincent Pierre Bertin makes no representations about the
-#	suitability of this software for any purpose.  It is provided "as is" without
-#	express or implied warranty.
-
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -47,3 +35,7 @@ user.save!
 #	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
 #	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
 #	local-part@domain
+#
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its documentation for any purpose and without fee is hereby granted, provided that the above copyright notice appear in all copies and that both that copyright notice and this permission notice appear in supporting documentation, and that the name of Karl Vincent Pierre Bertin not be used in advertising or publicity pertaining to distribution of the software without specific, written prior permission. Karl Vincent Pierre Bertin makes no representations about the suitability of this software for any purpose. It is provided "as is" without express or implied warranty.
