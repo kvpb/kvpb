cce39af6326fae0a62d542d2c653a2d883d4b110
Karl <local-part@domain>
2026-08-16 20:30:11 +0200

    (kvpb/db/migrate/) 20260815224940_create_articles.rb updated.

--- db/migrate/20260815224940_create_articles.rb
+++ db/migrate/20260815224940_create_articles.rb
@@ -1,4 +1,16 @@
-class CreateArticles < ActiveRecord::Migration[8.1]
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its
+#	documentation for any purpose and without fee is hereby granted, provided that
+#	the above copyright notice appear in all copies and that both that copyright
+#	notice and this permission notice appear in supporting documentation, and that
+#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+#	pertaining to distribution of the software without specific, written prior
+#	permission. Karl Vincent Pierre Bertin makes no representations about the
+#	suitability of this software for any purpose.  It is provided "as is" without
+#	express or implied warranty.
+
+class CreateArticles < ActiveRecord::Migration[ 8.1 ]
   def change
     create_table :articles do |t|
       t.string :kicker
@@ -16,3 +28,10 @@ class CreateArticles < ActiveRecord::Migration[8.1]
     add_index :articles, :published_at
   end
 end
+
+#	20260815224940_create_articles.rb
+#	kvpb.fr
+#
+#	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+#	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+#	local-part@domain
