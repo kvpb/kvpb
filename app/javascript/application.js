f28c264e1d765a9a25796e19e02a3710341e6c2e
Karl <local-part@domain>
2026-08-16 20:29:03 +0200

    (kvpb/app/javascript/) application.js updated.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
+//	Copyright 2026 by Karl Vincent Pierre Bertin
+//
+//	Permission to use, copy, modify, and distribute this software and its
+//	documentation for any purpose and without fee is hereby granted, provided that
+//	the above copyright notice appear in all copies and that both that copyright
+//	notice and this permission notice appear in supporting documentation, and that
+//	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+//	pertaining to distribution of the software without specific, written prior
+//	permission. Karl Vincent Pierre Bertin makes no representations about the
+//	suitability of this software for any purpose.  It is provided "as is" without
+//	express or implied warranty.
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//	application.js
+//	kvpb.fr
+//
+//	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+//	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+//	local-part@domain
