336b887fbb2c02c0e5b3b07737bdf0999696ca3e
Karl <local-part@domain>
2026-08-16 20:29:59 +0200

    (kvpb/app/mailers/) setting_mailer.rb created.

---
+++ app/mailers/setting_mailer.rb
@@ -0,0 +1,26 @@
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its
+#	documentation for any purpose and without fee is hereby granted, provided that
+#	the above copyright notice appear in all copies and that both that copyright
+#	notice and this permission notice appear in supporting documentation, and that
+#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+#	pertaining to distribution of the software without specific, written prior
+#	permission. Karl Vincent Pierre Bertin makes no representations about the
+#	suitability of this software for any purpose.  It is provided "as is" without
+#	express or implied warranty.
+
+class SettingMailer < ApplicationMailer
+  def login_token_rotated( setting )
+    @setting = setting
+
+    mail to: User.where( superuser: true ).pluck( :email_address ), subject: "Your new sign-in link"
+  end
+end
+
+#	setting_mailer.rb
+#	kvpb.fr
+#
+#	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+#	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+#	local-part@domain
