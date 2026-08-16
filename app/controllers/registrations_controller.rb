e80d93084a810e2e5c6e759573fdc2e536365809
Karl <local-part@domain>
2026-08-16 20:29:49 +0200

    (kvpb/app/controllers/) registrations_controller.rb updated.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,15 @@
+#	Copyright 2026 by Karl Vincent Pierre Bertin
+#
+#	Permission to use, copy, modify, and distribute this software and its
+#	documentation for any purpose and without fee is hereby granted, provided that
+#	the above copyright notice appear in all copies and that both that copyright
+#	notice and this permission notice appear in supporting documentation, and that
+#	the name of Karl Vincent Pierre Bertin not be used in advertising or publicity
+#	pertaining to distribution of the software without specific, written prior
+#	permission. Karl Vincent Pierre Bertin makes no representations about the
+#	suitability of this software for any purpose.  It is provided "as is" without
+#	express or implied warranty.
+
 class RegistrationsController < ApplicationController
   rate_limit to: 5, within: 1.hour, only: :create, with: -> { redirect_to new_registration_path, alert: "Try again later." }
 
@@ -15,7 +27,7 @@ class RegistrationsController < ApplicationController
       return
     end
 
-    @user = User.new(registration_params)
+    @user = User.new( registration_params )
 
     if @user.save
       start_new_session_for @user
@@ -27,6 +39,13 @@ class RegistrationsController < ApplicationController
 
   private
     def registration_params
-      params.require(:user).permit(:username, :email_address, :password, :password_confirmation)
+      params.require( :user ).permit( :username, :email_address, :password, :password_confirmation )
     end
 end
+
+#	registrations_controller.rb
+#	kvpb.fr
+#
+#	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
+#	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
+#	local-part@domain
